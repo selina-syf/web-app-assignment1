83d64eb9fc490fa6b049fc891cc186c5e214eceb
selina-syf <selinayifan@gmail.com>
2025-09-26 23:26:26 -0400

    Fixed reume link.

--- src/about.jsx
+++ src/about.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <p>My name is Yifan (Selina) Sun</p>
       <img src="/selina.jpeg" width='300px'/>
       <p>I am in the Software Engineering Technology ‐ Artificial Intelligence Co‐op program at Centennial College. I have learned C#, Python, Java, HTML, CSS, JavaScript, and React (MERN stack).</p>
-      <p>Here is my <a href='../public/resume.pdf'>resume</a></p>
+      <p>Here is my <a href='/resume.pdf'>resume</a></p>
     </div>
   );
 }
